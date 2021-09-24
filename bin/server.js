92708e39095738884c73191e103b1f001fbcce91
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-09-25 00:20:29 +0300

    Возвращает изменения

--- bin/server.js
+++ bin/server.js
@@ -1,19 +1,10 @@
 const app = require("../app");
-const mongoose = require("mongoose");
+const db = require("../model/db");
 
-require("dotenv").config();
-const { DB_HOST } = process.env;
+const PORT = process.env.PORT || 4000;
 
-mongoose
-  .connect(DB_HOST, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`);
-    });
-  })
-  .catch(() => process.exit(1));
-
-const { PORT = 3000 } = process.env;
+db.then(() => {
+  app.listen(PORT, () =>
+    console.log(`Server running. Use our API on port: ${PORT}`)
+  );
+}).catch((error) => console.log(error));
