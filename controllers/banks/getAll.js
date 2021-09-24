56c46f092c03c9e1a5bf729916514bcc53040b78
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-09-24 22:47:21 +0300

    Update getAll.js

--- controllers/banks/getAll.js
+++ controllers/banks/getAll.js
@@ -2,8 +2,9 @@ const { Bank } = require("../../model");
 
 const getAll = async (_, res, next) => {
   try {
-    const banks = await banksOperations.getAll();
-    res.json({ banks });
+    const result = await Bank.find();
+
+    res.json({ result });
   } catch (error) {
     next(error);
   }
