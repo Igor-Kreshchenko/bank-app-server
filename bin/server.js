0be5bea20b4f2b4c21e9b432bdcb424d5c26b858
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-09-24 23:40:52 +0300

    update

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,7 @@
 const app = require("../app");
 const db = require("../model/db");
 
-const { PORT = 3000 } = process.env;
+const PORT = process.env.PORT || 4000;
 
 db.then(() => {
   app.listen(PORT, () =>
